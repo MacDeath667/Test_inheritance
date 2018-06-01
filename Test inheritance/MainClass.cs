0ef2c68125f6094ab7d5913a9729c70d61df83bc
MacDeath667 <rom12.93@bk.ru>
2018-06-01 17:33:34 +0300

    Init

---
+++ Test inheritance/MainClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_inheritance;
+
+namespace Test_inheritance
+{
+    public class CaterpillarMilitary
+    {
+
+        public int ArmorThickness { get; set; }
+
+        public int Cost { get; set; }
+        public int speed = 0;
+        public int Speed { get; set; }
+        public string Country { get; set; }
+
+        public CaterpillarMilitary()
+        {
+            
+                Console.WriteLine(GetType().Name + " - base class");
+        
+        }
+
+    }
+}
+
+class Tank : CaterpillarMilitary
+{
+    public int Gun { get; set; }
+    public Tank()
+    {
+
+        Console.WriteLine(GetType().Name + " - daughter class");
+
+    }
+}
+
+class Artillery : CaterpillarMilitary
+{
+    public int ArtilleryGun { get; set; }
+}
+
---
+++ Test inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_inheritance;
+
+namespace Test_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank tigerII = new Tank() { Cost = 16000000, ArmorThickness = 160, Country = "Germany", Gun = 54, Speed = 39 };
+            Artillery su16 = new Artillery() { Cost = 420000, ArmorThickness = 24, Country = "USSR", ArtilleryGun = 68, Speed = 45 };
+            tigerII.speed = 3214123;
+
+            
+
+            
+            Console.ReadKey();
+        }
+    }
+}
